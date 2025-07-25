8af0f9afb435319e59b94ed3a1dd99e232a90f4c
Konstantin Nered <konstantin.n616@gmail.com>
2025-07-25 17:01:56 +0700

    configured sequelize and vite

---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+    res.end();
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+});
+
+module.exports = app;
