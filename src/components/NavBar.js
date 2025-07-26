8a9fcb133ccf45c221a5bffb5a5f90dbee287b77
Konstantin Nered <konstantin.n616@gmail.com>
2025-07-27 02:23:19 +0700

    updated navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,20 @@
-import { logout } from "../utils/utils"
+import { isLogged, logout } from "../utils/utils"
 
 export default function NavBar(){
+
+    setTimeout(() => {
+        const logoutBtn = document.getElementById('logout-btn');
+        if (!logoutBtn) return;
+
+        logoutBtn.addEventListener('click', () => logout());
+    }, 0)
+
     return (
         `
             <nav>
                 <button onclick="navigate('list')">Пользователи</button>
                 <button onclick="navigate('add')">Добавить</button>
-                <button onclick="logout">Выйти</button>
+                ${isLogged() ? `<button id="logout-btn">Выйти</button>` : `<button onclick="navigate('auth')">Вход</button>`}
             </nav>
         `
     )
