51d0d306554c8b6b3bb0b4c49f589e4251d833f3
Konstantin Nered <konstantin.n616@gmail.com>
2025-07-27 17:14:18 +0700

    finished userlist

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,4 +1,40 @@
+import { renderRoute } from "../utils/utils";
+
 export default function UserList(users = []) {
+
+    setTimeout(() => {
+        const deleteBtns = document.querySelectorAll('.delete');
+        const viewBtns = document.querySelectorAll('.view');
+        const editBtns = document.querySelectorAll('.edit');
+
+        if (!deleteBtns || !viewBtns || !editBtns) return;
+
+        deleteBtns.forEach(btn => btn.addEventListener('click', async () => {
+            const userId = btn.id.split('-')[1];
+            const response = await fetch(`http://localhost:3000/api/users/${userId}`, {
+                method: 'DELETE',
+            });
+            if (!response.ok) {
+                console.error('Error:', response.status);
+                return;
+            }
+            window.location.reload();
+        }));
+
+        viewBtns.forEach(btn => btn.addEventListener('click', () => {
+            const userId = btn.id.split('-')[1];
+            renderRoute('user_detail', userId);
+        }));
+
+        editBtns.forEach(btn => btn.addEventListener('click', () => {
+            const userId = btn.id.split('-')[1];
+            renderRoute('add', userId);
+        }));
+    }, 0);
+
+    console.log(users);
+
+
     return `
         <section id="user-list">
             <h2>Пользователи</h2>
@@ -16,11 +52,11 @@ export default function UserList(users = []) {
                         <tr>
                             <td>${user.username}</td>
                             <td>${user.first_name}</td>
-                            <td>${user.birthdate}</td>
+                            <td>${new Date(user.birthdate).toLocaleDateString()}</td>
                             <td>
-                                <button onclick="viewUser('${user.username}')">Подробнее</button>
-                                <button onclick="editUser('${user.username}')">Редактировать</button>
-                                <button onclick="deleteUser('${user.username}')">Удалить</button>
+                                <button class="view" id="view-${user.id}">Подробнее</button>
+                                <button class="edit" id="edit-${user.id}">Редактировать</button>
+                                <button class="delete" id="delete-${user.id}">Удалить</button>
                             </td>
                         </tr>
                     `).join('')}
